9b6daf9bd95899de2b72396f2349d4fc55eb4823
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-01-15 14:51:25 +0530

    Add files via upload

---
+++ telebot/plugins/g.py
@@ -0,0 +1,24 @@
+"""COMMAND : .g"""
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from telebot.utils import admin_cmd
+from telebot import CMD_HELP
+
+@telebot.on(admin_cmd("g"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "Abe Tu Gay tha Gay hai and Gay rhega and Tu Gand deta tha Gand deta hai and Tu Gandu Gand Deta rhega"
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f""
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()
+
+CMD_HELP.update( { "g": """**Plugin : dea Commands in degi are  • .da •  Function : **__Gali...__""" })
