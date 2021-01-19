7bb3f72aa07b0e0584739b4471b68e611a660d38
Sycrox <77438385+TheUnknownKanger@users.noreply.github.com>
2021-01-19 12:30:38 +0530

    Update updater.py

--- telebot/plugins/updater.py
+++ telebot/plugins/updater.py
@@ -24,7 +24,7 @@ requirements_path = path.join(
 
 HEROKU_API_KEY = Var.HEROKU_API_KEY
 HEROKU_APP_NAME = Var.HEROKU_APP_NAME
-GIT_REPO_NAME = "TeleBot"
+GIT_REPO_NAME = "XandarUserBot"
 UPSTREAM_REPO_URL = "https://github.com/TheUnknownKanger/XandarUserBot"
 
 xxxx = CMD_HNDLR if CMD_HNDLR else "."
@@ -84,12 +84,12 @@ async def upstream(ups):
         origin = repo.create_remote("upstream", off_repo)
         origin.fetch()
         force_updateme = True
-        repo.create_head("master", origin.refs.master)
+        repo.create_head("main", origin.refs.master)
         repo.heads.master.set_tracking_branch(origin.refs.master)
         repo.heads.master.checkout(True)
 
     ac_br = repo.active_branch.name
-    if ac_br != "master":
+    if ac_br != "main":
         await ups.edit(
             f"**[UPDATER]:**` Looks like you are using your own custom branch ({ac_br}). "
             "in that case, Updater is unable to identify "
@@ -179,7 +179,7 @@ async def upstream(ups):
         else:
             remote = repo.create_remote("heroku", heroku_git_url)
         try:
-            remote.push(refspec="HEAD:refs/heads/master", force=True)
+            remote.push(refspec="HEAD:refs/heads/main", force=True)
         except GitCommandError as error:
             await ups.edit(f"{txt}\n`Here is the error log:\n{error}`")
             repo.__del__()
